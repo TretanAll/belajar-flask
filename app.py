19e1d19735338f2a7a0f83054c06ff90634c807f
Tretan <fata.fatchur2906@gmail.com>
2019-05-08 10:11:53 +0700

    Tes templates index.html

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
-from flask  import Flask
+from flask  import Flask, render_template
 app = Flask(__name__)
 
 #: route untuk index
 @app.route('/')
 def hello():
-    return 'Hello Bosku!!!'
+    #:memanggil templates yang sudah dibuat
+    return render_template('index.html')
 
-#: route menambahkan url biasa
-@app.route('/setting')
-def setting():
-    return 'Hello Bosku ini di url setting!!!'
     
 #: route untuk url tambahan + parameter
 @app.route('/profile/<username>')
 def show_profile(username):
     #: tanda %s untuk tipe data string
     return 'Hello %s' % username + '!!!'
-    
-#: route untuk url tambahan + tipe data parameter
-@app.route('/show_id/<int:blog_id>')
-def show_blog(blog_id):
-    #: tanda %d untuk tipe data angka
-    return 'Hello ID anda =   %d' % blog_id
